ff39a2a1edadd4ff05f21c787874b8479e64a6c0
Eloi Perdereau <eloi.perdereau@imt-atlantique.fr>
2024-03-26 20:58:43 +0100

    Revert "vote/app.py and worker/Program.cs updates for the Redis on OS VM"
    This reverts commit e1c98b97ca863801e098af321b768c9e5e24ba21.

--- worker/Program.cs
+++ worker/Program.cs
@@ -113,7 +113,7 @@ namespace Worker
                 try
                 {
                     Console.Error.WriteLine("Connecting to redis");
-                    return ConnectionMultiplexer.Connect("redis,password=changeit0");
+                    return ConnectionMultiplexer.Connect(ipAddress);
                 }
                 catch (RedisConnectionException)
                 {
